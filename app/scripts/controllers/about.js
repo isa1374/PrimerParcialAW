3869fbd1a97eca2a421334a0180a9db5a2880879
Isa <isha13gp@hotmail.com>
2016-09-06 20:02:57 -0500

    Let's start with the basics

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name primerApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the primerApp
+ */
+angular.module('primerApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name primerApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the primerApp
+ */
+angular.module('primerApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
