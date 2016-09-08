929df9b8f72711f74660d6d31a90316fd2546c76
Isa <isha13gp@hotmail.com>
2016-09-08 17:22:06 -0500

    comienzo de funcion.....(no terminado)

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,4 +2,19 @@ var modelo={};
 var primerApp= angular.module("primerApp", []);
 primerApp.controller("primerAppController", function($scope){
     $scope.conversiones=modelo;
+    $scope.nuevaDivisa=function(paisOrigen, valorOrigen, paisConv, valorConv){
+        
+        $scope.divisas.acciones.push({paisOrigen: paisOrigen, valorOrigen: valorOrigen, paisConv:paisConv, valorConv:valorConv});
+        
+        
+        $scope.calcular(opcion, valor){
+            
+            function(item){
+                   
+            }
+            
+            
+        }
+        
+    }
 });
