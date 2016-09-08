0c6667970ede99e64d64df096fbebbfcbc8c39cb
loresfca <loresfca.flores1@gmail.cpm>
2016-09-07 21:20:29 -0500

    ya estan los componentes bonitos (odio los checkboxes)

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,4 +1,4 @@
-var modelo={};
+var modelo={base:'USD',valores:[{nombre:'MXN', valor:'18.38'},{nombre:'CAD', valor:'1.29'},{nombre:'JPY', valor:'101.73'},{nombre:'GBP', valor:'0.75'},{nombre:'EUR', valor:'0.89'}],resultado:[{}]};
 var primerApp= angular.module("primerApp", []);
 primerApp.controller("primerAppController", function($scope){
     $scope.conversiones=modelo;
