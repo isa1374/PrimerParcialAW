cf19ab998f0ce48b56d4dc65f72fa1e1b71cf99d
loresfca <loresfca.flores1@gmail.cpm>
2016-09-09 14:27:17 -0500

    banderas

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,7 +4,7 @@ var modelo={base:'USD',
           {nombre:'MXN', valor:'18.83'},
           {nombre:'CAD', valor:'1.3'},
           {nombre:'JPY', valor:'102.87'},
-          {nombre:'GBP', valor:'0.75'},
+          {nombre:'GBP', valor:'0.7543'},
           {nombre:'EUR', valor:'0.89'}],
  resultado:[]};
 /*primerApp.run(function($http){
@@ -25,42 +25,42 @@ primerApp.controller("primerAppController", function($scope,$filter){
             mx = $filter('filter')($scope.conversiones.valores,{nombre:'MXN'});
             var mxnVal=mx[0].valor;
             var valorRes=Math.floor(valor/orVal*mxnVal*1000)/1000;
-            $scope.conversiones.resultado.push({nombre:'MXN', valor:valorRes});
+            $scope.conversiones.resultado.push({imagen:'./images/m.png' ,nombre:'MXN',valor:valorRes});
         }
         if(usdRes){
             var mx=[];
             mx = $filter('filter')($scope.conversiones.valores,{nombre:'USD'});
             var mxnVal=mx[0].valor;
             var valorRes=Math.floor(valor/orVal*mxnVal*1000)/1000;
-            $scope.conversiones.resultado.push({nombre:'USD', valor:valorRes});
+            $scope.conversiones.resultado.push({imagen:'./images/us.png',nombre:'USD', valor:valorRes});
         }
         if(eurRes){
             var mx=[];
             mx = $filter('filter')($scope.conversiones.valores,{nombre:'EUR'});
             var mxnVal=mx[0].valor;
             var valorRes=Math.floor(valor/orVal*mxnVal*1000)/1000;
-            $scope.conversiones.resultado.push({nombre:'EUR', valor:valorRes});
+            $scope.conversiones.resultado.push({imagen:'./images/eu.png',nombre:'EUR', valor:valorRes});
         }
         if(jpyRes){
             var mx=[];
             mx = $filter('filter')($scope.conversiones.valores,{nombre:'JPY'});
             var mxnVal=mx[0].valor;
             var valorRes=Math.floor(valor/orVal*mxnVal*1000)/1000;
-            $scope.conversiones.resultado.push({nombre:'JPY', valor:valorRes});
+            $scope.conversiones.resultado.push({imagen:'./images/j.png',nombre:'JPY', valor:valorRes});
         }
         if(cadRes){
             var mx=[];
             mx = $filter('filter')($scope.conversiones.valores,{nombre:'CAD'});
             var mxnVal=mx[0].valor;
             var valorRes=Math.floor(valor/orVal*mxnVal*1000)/1000;
-            $scope.conversiones.resultado.push({nombre:'CAD', valor:valorRes});
+            $scope.conversiones.resultado.push({imagen:'./images/c.png',nombre:'CAD', valor:valorRes});
         }
         if(gbpRes){
             var mx=[];
             mx = $filter('filter')($scope.conversiones.valores,{nombre:'GBP'});
             var mxnVal=mx[0].valor;
             var valorRes=Math.floor(valor/orVal*mxnVal*1000)/1000;
-            $scope.conversiones.resultado.push({nombre:'GBP', valor:valorRes});
+            $scope.conversiones.resultado.push({imagen:'./images/uk.png',nombre:'GBP', valor:valorRes});
         }
         
     }
