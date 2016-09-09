f823802aea98404bdfe6e1778ae2d7d1b022b3fc
Isa <isha13gp@hotmail.com>
2016-09-09 10:12:00 -0500

    conversion

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,24 +1,29 @@
-var modelo={base:'USD',valores:[{nombre:'MXN', valor:'18.38'},{nombre:'CAD', valor:'1.29'},{nombre:'JPY', valor:'101.73'},{nombre:'GBP', valor:'0.75'},{nombre:'EUR', valor:'0.89'}],resultado:[{}]};
+var modelo={base:'USD',valores:[{nombre:'MXN', valor:'18.38'},{nombre:'CAD', valor:'1.29'},{nombre:'JPY', valor:'101.73'},{nombre:'GBP', valor:'0.75'},{nombre:'EUR', valor:'0.89'}],resultado:[{resultado: 0.0}]}; 
 var primerApp= angular.module("primerApp", []);
 primerApp.controller("primerAppController", function($scope){
     $scope.conversiones=modelo;
-<<<<<<< HEAD
-    $scope.nuevaDivisa=function(paisOrigen, valorOrigen, paisConv, valorConv){
-        
-        $scope.divisas.acciones.push({paisOrigen: paisOrigen, valorOrigen: valorOrigen, paisConv:paisConv, valorConv:valorConv});
-        
-        
-        $scope.calcular(opcion, valor){
-            
-            function(item){
-                   
-            }
-            
-            
-        }
-        
+    $scope.nuevaconv=function(nombre, resultado){
+        $scope.conversiones.valores.push({nombre:nombre}); 
+        $scope.conversiones.resultado.push({resultado:resultado}); 
     }
+    $scope.calcular=function(original, cantidad, convertir){
+        var resp;
+        angular.forEach($scope.conversiones.valores, function(it){
+            if(it.nombre.includes(origional)){
+                angular.forEach($scope.conversiones.valores, function(item){
+                     if(item.nombre.includes(convertir)){
+                            resp = ((cantidad/it.valor)/item.valor);
+                //cantidad entre valor en dolares del elegido, entre valor en dolares de la conversión 
+                         $scope.conversiones.valores.push({nombre:convertir});
+                         $scope.conversiones.resultado.push({resultado:resp});
+                            
+                    }
+                });
+            }
+        });
+    }
+
 });
-=======
-});
->>>>>>> origin/master
+
+
+
