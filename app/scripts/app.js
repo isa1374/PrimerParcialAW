e5c5548b55ceaad60caa090e10186511b2558f2e
loresfca <loresfca.flores1@gmail.cpm>
2016-09-09 12:08:33 -0500

    Ya funciona bien padre

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,28 +1,69 @@
-var modelo={base:'USD',valores:[{nombre:'MXN', valor:'18.38'},{nombre:'CAD', valor:'1.29'},{nombre:'JPY', valor:'101.73'},{nombre:'GBP', valor:'0.75'},{nombre:'EUR', valor:'0.89'}],resultado:[{resultado: 0.0}]}; 
 var primerApp= angular.module("primerApp", []);
-primerApp.controller("primerAppController", function($scope){
+var modelo={base:'USD',
+ valores:[{nombre:'USD', valor:'1'},
+          {nombre:'MXN', valor:'18.4'},
+          {nombre:'CAD', valor:'1.3'},
+          {nombre:'JPY', valor:'101.73'},
+          {nombre:'GBP', valor:'0.75'},
+          {nombre:'EUR', valor:'0.89'}],
+ resultado:[]};
+/*primerApp.run(function($http){
+    $http.get('./scripts/datos.json').success(function(data){
+        $scope.conversiones = data;
+    });
+});*/
+primerApp.controller("primerAppController", function($scope,$filter){
+    $scope.valor = 1;
     $scope.conversiones=modelo;
-    $scope.nuevaconv=function(nombre, resultado){
-        $scope.conversiones.valores.push({nombre:nombre}); 
-        $scope.conversiones.resultado.push({resultado:resultado}); 
+    $scope.calcular=function(valor,original,mxnRes,usdRes,eurRes,jpyRes,cadRes,gbpRes){
+        $scope.conversiones.resultado=[];
+        var or=[];
+        or=$filter('filter')($scope.conversiones.valores,{nombre:original});
+        var orVal=or[0].valor;
+        if(mxnRes){
+            var mx=[];
+            mx = $filter('filter')($scope.conversiones.valores,{nombre:'MXN'});
+            var mxnVal=mx[0].valor;
+            var valorRes=Math.floor(valor/orVal*mxnVal*1000)/1000;
+            $scope.conversiones.resultado.push({nombre:'MXN', valor:valorRes});
+        }
+        if(usdRes){
+            var mx=[];
+            mx = $filter('filter')($scope.conversiones.valores,{nombre:'USD'});
+            var mxnVal=mx[0].valor;
+            var valorRes=Math.floor(valor/orVal*mxnVal*1000)/1000;
+            $scope.conversiones.resultado.push({nombre:'USD', valor:valorRes});
+        }
+        if(eurRes){
+            var mx=[];
+            mx = $filter('filter')($scope.conversiones.valores,{nombre:'EUR'});
+            var mxnVal=mx[0].valor;
+            var valorRes=Math.floor(valor/orVal*mxnVal*1000)/1000;
+            $scope.conversiones.resultado.push({nombre:'EUR', valor:valorRes});
+        }
+        if(jpyRes){
+            var mx=[];
+            mx = $filter('filter')($scope.conversiones.valores,{nombre:'JPY'});
+            var mxnVal=mx[0].valor;
+            var valorRes=Math.floor(valor/orVal*mxnVal*1000)/1000;
+            $scope.conversiones.resultado.push({nombre:'JPY', valor:valorRes});
+        }
+        if(cadRes){
+            var mx=[];
+            mx = $filter('filter')($scope.conversiones.valores,{nombre:'CAD'});
+            var mxnVal=mx[0].valor;
+            var valorRes=Math.floor(valor/orVal*mxnVal*1000)/1000;
+            $scope.conversiones.resultado.push({nombre:'CAD', valor:valorRes});
+        }
+        if(gbpRes){
+            var mx=[];
+            mx = $filter('filter')($scope.conversiones.valores,{nombre:'GBP'});
+            var mxnVal=mx[0].valor;
+            var valorRes=Math.floor(valor/orVal*mxnVal*1000)/1000;
+            $scope.conversiones.resultado.push({nombre:'GBP', valor:valorRes});
+        }
+        
     }
-    $scope.calcular=function(original, cantidad, convertir){
-        var resp;
-        angular.forEach($scope.conversiones.valores, function(it){
-            if(it.nombre.includes(origional)){
-                angular.forEach($scope.conversiones.valores, function(item){
-                     if(item.nombre.includes(convertir)){
-                            resp = ((cantidad/it.valor)/item.valor);
-                //cantidad entre valor en dolares del elegido, entre valor en dolares de la conversión 
-                         $scope.conversiones.valores.push({nombre:convertir});
-                         $scope.conversiones.resultado.push({resultado:resp});
-                            
-                    }
-                });
-            }
-        });
-    }
-
 });
 
 
